794e10d0b8a705ae0d65fea53f640bf37e00c54d
ARTHUR LUCAS LIMA <15110432@pucrs.br>
2018-09-19 20:50:58 -0300

    entrega do trabalho

---
+++ trabalho4,30.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using
+ namespace std;
+
+
+int main()
+ {
+
+double D;
+
+double A;
+
+double r;
+
+  cout << "diga o raio!\n";
+
+   cin >> r;
+ 
+  
+   cout << " O valor do diametro é  ";
+ 
+  D=r*2;
+ 
+  cout << D << endl;
+  
+  cout << " O valor da area é  ";
+
+   A=r*3.14159*2;
+
+   cout << A;
+
+
+
+return 0;
+
+
+}
+
