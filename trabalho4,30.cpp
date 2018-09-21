5e67f8095749732d93db905c14dfdd65612d9555
ArthurLLima97 <43223050+ArthurLLima97@users.noreply.github.com>
2018-09-21 20:18:12 -0300

    Update trabalho4,30.cpp

--- trabalho4,30.cpp
+++ trabalho4,30.cpp
@@ -1,38 +1,25 @@
-﻿#include <iostream>
+#include <iostream>
+#include <math.h>
 using
- namespace std;
-
-
+namespace std;
 int main()
  {
-
 double D;
-
+double C;
 double A;
-
 double r;
-
   cout << "diga o raio!\n";
-
    cin >> r;
- 
-  
    cout << " O valor do diametro é  ";
- 
   D=r*2;
- 
   cout << D << endl;
-  
+  cout << " O valor da Circunferenia é  ";
+   C=r*3.14159*2;
+   cout << C << endl;
   cout << " O valor da area é  ";
-
-   A=r*3.14159*2;
-
-   cout << A;
-
-
-
+   
+   A=pow(r,2)*3.14159;
+   cout << A<< endl;
 return 0;
-
-
 }
 
